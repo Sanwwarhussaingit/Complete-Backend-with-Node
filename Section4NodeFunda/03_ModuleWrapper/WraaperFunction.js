6f197bad425c3c50b3e15d67b576ba1c392d2356
Sanwwar Hussain <154716460+Sanwwarhussaingit@users.noreply.github.com>
2024-10-02 11:14:57 +0530

    module Wrapper

---
+++ Section4NodeFunda/03_ModuleWrapper/WraaperFunction.js
@@ -0,0 +1,6 @@
+(function (exports, require, module, __filename, __dirname) {
+   //module code goes here
+//    const a = 10
+//     console.log(a);
+    
+});
---
+++ Section4NodeFunda/03_ModuleWrapper/app.js
@@ -0,0 +1,8 @@
+const m = require('./math')
+//     a = 10
+//     console.log(a);
+
+
+
+console.log(__dirname);
+console.log(__filename);
