10f8843e67cc03201034133c868344e98e5682de
Sanwwar <sanwwarcode@gmail.com>
2024-10-02 10:03:21 +0530

    add section2 of operating system basics

---
+++ Section2/1IntroductionToTerminal/app.js
@@ -0,0 +1,10 @@
+let a=20
+let b=30
+function add(a,b){
+	console.log(a+b)
+}
+console.log(10+2)
+add(a,b)
+console.log("hi");
+
+
---
+++ Section2/1IntroductionToTerminal/test.sh
@@ -0,0 +1,6 @@
+# num1=5
+# num2=10
+# echo $((num1+num2))
+# echo "End"
+
+# for i in {0..99}; do rm "app[$i].js"; done
